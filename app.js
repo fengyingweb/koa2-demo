249dc5328c403aa5d923afa292cfc4a971161442
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-01 21:49:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router')
 const app = new Koa();
-app.use(bodyParser());
-app.use(async (ctx)=> {
-  // 从request中获取get请求
-  let url = ctx.url;
-  let method = ctx.method;
-  if (url === '/' && method === 'GET') {
-    let request = ctx.request;
-    let req_query = request.query;
-    let req_queryString = request.querystring;
+const router = new Router();
+const {mongoose, connect, initSchemas} = require('./dataBase/init.js');
 
-    // 从执行上线文中获取ctx
-    let ctx_query = ctx.query;
-    let ctx_queryString = ctx.querystring;
-    ctx.body = {
-      url,
-      method,
-      req_query,
-      req_queryString,
-      ctx_query,
-      ctx_queryString
-    }
-  } else if (url === '/' && method === 'POST') {
-    const parseData = ctx.request.body;
-    console.log(parseData)
-    ctx.body = parseData;
-  } else {
-    ctx.body = '404'
-  }
+(async ()=> {
+  await connect();
+  initSchemas();
+  const User = mongoose.model('User');
+  let oneUser = new User({userName:'jspang',password:'123456'});
+  oneUser.save().then(async ()=> {
+    console.log('插入数据成功');
+    let users = await User.findOne({}).exec();
+    console.log('------------------')
+    console.log(users)
+    console.log('------------------')
+  })
+})();
+app.use(bodyParser());
+
+router.get('/', (ctx, next)=> {
+  ctx.body = {
+    method: ctx.method,
+    ...ctx.query
+  };
 })
 
-function parsePostData(ctx) {
-  return new Promise((resolve, reject)=> {
-    try{
-      let parseData = ''
-      ctx.req.on('data', (data)=> {
-        console.log(data)
-        parseData += data
-      });
+router.post('/:id', (ctx, next)=> {
+  ctx.body = {
+    method: ctx.method,
+    ...ctx.request.body,
+    ...ctx.params
+  };
+})
+// app.use(async (ctx)=> {
+//   let method = ctx.request.method;
+//   if (method === 'GET') {
+//     ctx.body = router
+//   } else if (method === 'POST') {
+//     ctx.body = ctx.request.body;
+//   }
+// })
 
-      ctx.req.addListener('end', ()=> {
-        resolve(parseData)
-      })
-    }catch(error) {
-      reject(error)
-    }
-  })
-}
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3001, ()=> {
-  console.log('start listening port 3001')
+  console.log('start listening port 3000')
 })
