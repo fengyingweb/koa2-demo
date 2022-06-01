5d1c3f06b1f6e1864a1ebd1ff5a35372240a552b
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-01 21:48:13 +0800

    Update User.js

--- dataBase/schema/User.js
+++ dataBase/schema/User.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //声明schema
+const ObjectId = Schema.Types.ObjectId; // 声明Object类型
 
+const userSchema = new Schema({
+  userId: ObjectId,
+  userName: {unique: true, type: String}, // unique: 布尔值 是否对这个属性创建唯一索引
+  password: String,
+  createTime: {type: Date, default: Date.now()},
+  lastLoginTime: {type: Date, default: Date.now()}
+});
+
+// 发布模型
+mongoose.model('User', userSchema);
