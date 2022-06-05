d2f1c67d7d08cfdcc29605f6c9aed1b3c7391afe
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-05 21:18:54 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const cors = require('koa2-cors'); // 引入跨域中间件
-const Router = require('koa-router');
+const cors = require('koa2-cors'); // 引入跨域
+const Router = require('koa-router')
 const app = new Koa();
-// const mongoose = require('mongoose');
-// const {connect, initSchemas} = require('./dataBase/init.js');
+const {connect, initSchemas} = require('./dataBase/init.js');
 
-let router = new Router({
+app.use(bodyParser());
+app.use(cors());
+
+const router = new Router({
   prefix: '/api'
 });
-// 引入子路由
-const user = require('./appApi/user.js');
+
+// 子路由
+const user = require('./api/user.js');
 
 // 装载所有子路由
-router.use('/user', user.routes())
+router.use('/user', user.routes());
 
-// (async ()=> {
-//   await connect();
-//   initSchemas();
-//   const User = mongoose.model('User');
-//   let oneUser = new User({userName:'jspang',password:'123456'});
-//   oneUser.save().then(async ()=> {
-//     console.log('插入数据成功')
-//     let users = await User.findOne({}).exec()
-//     console.log('------------------')
-//     console.log(users)
-//     console.log('------------------')
-//   });
-// })();
-
-app.use(cors()); // 配置跨域
-app.use(bodyParser()); // 配置body，post请求时需要使用ctx.request.body
-
+// 加载路由中间件
 app
   .use(router.routes())
   .use(router.allowedMethods())
+
+
+;(async ()=> {
+  await connect();
+  initSchemas();
+})();
+
+// 错误处理
+app.use(async(ctx, next)=> {
+  try {
+    await next();
+    if (!ctx.body) {
+      ctx.status = 404;
+      ctx.body = {
+        code: 404,
+        msg: 'not found'
+      }
+    }
+  } catch(err) {
+    console.log(err);
+    ctx.status = 500
+    ctx.body = {
+      code: 500,
+      msg: 'server error'
+    }
+  }
+})
 
 app.listen(3001, ()=> {
   console.log('start listening port 3001')
