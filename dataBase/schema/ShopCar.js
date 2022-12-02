e8687a471085a95f43ecd6f13a0ca1c6c374c7bb
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-12-02 18:04:16 +0800

    Add files via upload

---
+++ dataBase/schema/ShopCar.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const shopCarSchema = new Schema({
+  shopCarId:{type: ObjectId},
+  goodsId: String,
+  name: String,
+  price: Number,
+  image: String,
+  goodsNums: Number,
+  userId: String
+}, {collection: 'shopCar'})
+
+mongoose.model('ShopCar', shopCarSchema)
